41c09b59e1b5f1da80d463ee73e64616ebb64166
Aadithya Sai <aadithyas0610@gmail.com>
2025-01-26 10:57:20 +0530

    setup alerts for errors

--- src/auth/components/Signup.jsx
+++ src/auth/components/Signup.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import "../AuthenticationForm.css";
+import Alert from "../../common/Alert";
 
 const Signup = ({ onSwitch }) => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [alertData, setAlertData] = useState({
+    active: false,
+    message: "",
+    type: "",
+  });
 
   const handleSignup = (e) => {
     e.preventDefault();
-    // const user = { name, email, password };
     const user = { email, password };
     console.log(import.meta.env.VITE_HOBB_BACKEND_URL);
     fetch(import.meta.env.VITE_HOBB_BACKEND_URL + "/users", {
@@ -18,18 +23,43 @@ const Signup = ({ onSwitch }) => {
       },
       body: JSON.stringify(user),
     })
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          setAlertData({
+            active: true,
+            message: "Bad credentials! Please try again.",
+            type: "danger",
+          });
+        }
+        return res.json();
+      })
       .then((data) => {
         if (data.error) {
           alert(data.error);
         } else {
           window.location.reload();
         }
+      })
+      .catch((error) => {
+        if (error instanceof TypeError) {
+          console.error("Network error or CORS issue:", error.message);
+          setAlertData({
+            active: true,
+            message: "Network Error! Please try again later.",
+            type: "danger",
+          });
+        } else {
+          console.error("Error:", error.message);
+        }
       });
   };
 
   return (
     <div className="mainbox">
+      <Alert
+        {...alertData}
+        setInactive={() => setAlertData({ active: false })}
+      />
       <div className="form-container">
         <h2>Sign Up</h2>
         <form onSubmit={handleSignup}>

639330ff5d5b7e84b5748c0ce253cb85d29f3ed8
Aadithya Sai <aadithyas0610@gmail.com>
2025-01-26 10:57:05 +0530

    formatting

--- src/auth/AuthenticationForm.jsx
+++ src/auth/AuthenticationForm.jsx
@@ -3,10 +3,9 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 import ForgotPassword from "./components/ForgotPassword";
 import "./AuthenticationForm.css";
-import logo from "./components/logo.jpg";
+import logo from "../assets/logo.jpg";
 import Waves from "./components/Waves";
 
-    
 const AuthenticationForm = () => {
   const [currentForm, setCurrentForm] = useState("login");
 
@@ -15,36 +14,33 @@ const AuthenticationForm = () => {
   const switchToForgotPassword = () => setCurrentForm("forgotPassword");
 
   return (
-
-    
     <div className="app-container">
-
-<Waves
-lineColor="#fff"
-backgroundColor="rgba(255, 255, 255, 0.2)"
-waveSpeedX={0.02}
-waveSpeedY={0.01}
-waveAmpX={40}
-waveAmpY={20}
-friction={0.9}
-tension={0.01}
-maxCursorMove={120}
-xGap={12}
-yGap={36}
-/>
+      <Waves
+        lineColor="#fff"
+        backgroundColor="rgba(255, 255, 255, 0.2)"
+        waveSpeedX={0.02}
+        waveSpeedY={0.01}
+        waveAmpX={40}
+        waveAmpY={20}
+        friction={0.9}
+        tension={0.01}
+        maxCursorMove={120}
+        xGap={12}
+        yGap={36}
+      />
       <div className="align-containor">
-      <img src={logo} alt="Logo" className="logo" />
-      
-      {currentForm === "login" && (
-         <Login
-           onSwitch={switchToSignup}
-           onForgotPassword={switchToForgotPassword}
-         />
-       )}
-       {currentForm === "signup" && <Signup onSwitch={switchToLogin} />}
-       {currentForm === "forgotPassword" && (
-         <ForgotPassword onSwitch={switchToLogin} />
-       )}
+        <img src={logo} alt="Logo" className="logo" />
+
+        {currentForm === "login" && (
+          <Login
+            onSwitch={switchToSignup}
+            onForgotPassword={switchToForgotPassword}
+          />
+        )}
+        {currentForm === "signup" && <Signup onSwitch={switchToLogin} />}
+        {currentForm === "forgotPassword" && (
+          <ForgotPassword onSwitch={switchToLogin} />
+        )}
       </div>
     </div>
   );
